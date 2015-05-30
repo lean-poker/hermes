dfbe3148062f26aff979fad05d30b49c531d245c
devill <devillmeister@gmail.com>
2015-05-30 14:06:15 +0200

    Simple token based auth

--- app.rb
+++ app.rb
@@ -24,30 +24,35 @@ post '/deployment' do
 end
 
 patch '/deployment/:id' do
+  halt 401 unless params[:token] == ENV['TOKEN']
   LeanPokerHermes::Workers::Deploy.perform_async(params[:id], params[:owner], params[:repository], params[:commit], params[:callback_url])
   JSON.generate :success => true
 end
 
 post '/deployment/:id/log_drain' do
-  p "Adding log drain for #{params[:id]} with url #{params[:url]}"
+  puts "Adding log drain for #{params[:id]} with url #{params[:url]}"
   id = LeanPokerHermes::HerokuGateway.instance.add_log_drain(params[:id], params[:url])
   JSON.generate({:success => true, :id => id})
 end
 
 delete '/deployment/:id/log_drain/:drain_id' do
+  halt 401 unless params[:token] == ENV['TOKEN']
   LeanPokerHermes::HerokuGateway.instance.delete_log_drain(params[:id], params[:drain_id])
   JSON.generate :success => true
 end
 
 delete '/deployment/:id' do
+  halt 401 unless params[:token] == ENV['TOKEN']
   LeanPokerHermes::Workers::Delete.perform_async(params[:id])
   JSON.generate :success => true
 end
 
 get '/deployment/:id/config_var' do
+  halt 401 unless params[:token] == ENV['TOKEN']
   JSON.generate LeanPokerHermes::HerokuGateway.instance.get_config_vars(params[:id])
 end
 
 patch '/deployment/:id/config_var' do
+  halt 401 unless params[:token] == ENV['TOKEN']
   JSON.generate LeanPokerHermes::HerokuGateway.instance.set_config_vars(params[:id], JSON.parse(params[:config_vars]))
 end
