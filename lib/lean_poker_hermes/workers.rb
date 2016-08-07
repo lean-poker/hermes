5ee5604999be5bb662b9ed4033a6b668a5c0973a
devill <devillmeister@gmail.com>
2016-08-07 16:34:44 +0200

    Add missing autoloads

--- lib/lean_poker_hermes/workers.rb
+++ lib/lean_poker_hermes/workers.rb
@@ -3,6 +3,8 @@ module LeanPokerHermes::Workers
   autoload :Create, 'lean_poker_hermes/workers/create'
   autoload :Delete, 'lean_poker_hermes/workers/delete'
   autoload :Deploy, 'lean_poker_hermes/workers/deploy'
+  autoload :RespondToCallback, 'lean_poker_hermes/workers/respond_to_callback'
+  autoload :SetupEnvironmentVariables, 'lean_poker_hermes/workers/setup_environment_variables'
 end
 
 
