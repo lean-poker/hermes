5cd357dec2d0bcdb08510200f6a8454036fb65b5
DeVill <devillmeister@gmail.com>
2018-11-30 14:46:18 +0100

    remove deployment check for now

--- lib/lean_poker_hermes/workers/deploy.rb
+++ lib/lean_poker_hermes/workers/deploy.rb
@@ -7,23 +7,23 @@ class LeanPokerHermes::Workers::Deploy
   def perform(id, owner, repository, archive_url, commit, callback_url, target_heroku_api_key)
     deploy = LeanPokerHermes::HerokuGateway.instance(target_heroku_api_key).deploy(id, archive_url, commit)
 
-    begin
-      sleep 15
-      info = LeanPokerHermes::HerokuGateway.instance(target_heroku_api_key).deployment_result(id,deploy['id'])
-    end while info['build']['status'] == 'pending'
+    #begin
+    #  sleep 15
+    #  info = LeanPokerHermes::HerokuGateway.instance(target_heroku_api_key).deployment_result(id,deploy['id'])
+    #end while info['build']['status'] == 'pending'
 
-    success = (info['build']['status'] == 'succeeded') ? '1' : '0'
-    logs = info['lines'].map { |line| line['line'] }.join('')
+    #success = (info['build']['status'] == 'succeeded') ? '1' : '0'
+    #logs = info['lines'].map { |line| line['line'] }.join('')
 
-    info = {
-        :id => id,
-        :owner => owner,
-        :repository => repository,
-        :commit => commit,
-        :success => success,
-        :logs => logs
-    }
+    #info = {
+    #    :id => id,
+    #    :owner => owner,
+    #    :repository => repository,
+    #    :commit => commit,
+    #    :success => success,
+    #    :logs => logs
+    #}
 
-    LeanPokerHermes::Workers::RespondToCallback.perform_async(callback_url, info)
+    #LeanPokerHermes::Workers::RespondToCallback.perform_async(callback_url, info)
   end
 end
