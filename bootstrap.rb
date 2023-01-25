47cf0ac7de21655aca2cbf4b8aa15349dfe8712c
Nathan Le Ray <nathan@sxnlabs.com>
2023-01-25 19:37:40 +0100

    Cleanup

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,20 +1,22 @@
-require 'dotenv'
-require 'platform-api'
-require 'sidekiq'
-require_relative 'lib/lean_poker_hermes'
+require "dotenv"
+require "platform-api"
+require "sidekiq"
+require_relative "lib/lean_poker_hermes"
 
 Dotenv.load
 
+# Callback payloads are problematic.
+# Probably because of the logs.
+Sidekiq.strict_args!(false)
+
 Sidekiq.configure_client do |config|
   config.redis = {
-      :url => ENV['REDISCLOUD_URL'],
-      :namespace => 'LeanPokerHermes'
+    url: ENV["REDISCLOUD_URL"]
   }
 end
 
 Sidekiq.configure_server do |config|
   config.redis = {
-      :url => ENV['REDISCLOUD_URL'],
-      :namespace => 'LeanPokerHermes'
+    url: ENV["REDISCLOUD_URL"]
   }
 end
